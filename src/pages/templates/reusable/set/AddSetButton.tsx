4c0f01b7e6ceaa51ac73b3fce78324b3014b36bb
funcnroll <115594042+funcnroll@users.noreply.github.com>
2025-10-24 16:47:52 +0200

    Stylistic changes

--- src/pages/templates/reusable/set/AddSetButton.tsx
+++ src/pages/templates/reusable/set/AddSetButton.tsx
@@ -6,7 +6,7 @@ function AddSetButton({ onClick }: AddSetButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex items-center justify-center w-full gap-2 py-3 mt-4 font-medium transition duration-200 rounded-lg cursor-pointer bg-textDisabled hover:bg-borderDefault text-textSecondary hover:text-textPrimary"
+      className="flex items-center justify-center w-full gap-2 py-3 mt-4 font-medium transition duration-200 rounded-lg cursor-pointer bg-secondaryColor hover:bg-borderDefault text-textPrimary "
     >
       <span className="text-lg">+</span>
       Add Set
