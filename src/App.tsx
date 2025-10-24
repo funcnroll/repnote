b954d9b5628633dbaaadcf83550f3b6634e267e8
funcnroll <115594042+funcnroll@users.noreply.github.com>
2025-10-24 17:29:09 +0200

    Reverse all safari specific nav changes #2

--- src/App.tsx
+++ src/App.tsx
@@ -80,10 +80,7 @@ export default function App() {
           </div>
 
           {name && (
-            <div
-              className="fixed bottom-0 left-0 w-full sm:absolute"
-              style={{ paddingBottom: "env(safe-area-inset-bottom)" }}
-            >
+            <div className="fixed bottom-0 left-0 w-full sm:absolute">
               <NavMenu />
             </div>
           )}
